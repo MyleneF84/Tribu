0e900455466f5905273c1741f58e203cf0eb9701
Mylene Febrissy <m.febrissy@gmail.com>
2022-05-30 15:01:32 +0200

    User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :bookings
+  has_many :events, through: :bookings
+  has_many :proposed_events, through: :bookings, source: :events, dependent: :destroy
+  validates :first_name, :last_name, uniqueness: true, presence: true
+  validates :email, presence: true, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  }
 end
