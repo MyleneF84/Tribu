f7e2605a971394e41b16fe1294104552a4d20783
ophelia ondella <ophelia.ondella@gmail.com>
2022-05-31 14:26:34 +0200

    Rajout 3 Booking Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-
-
+Booking.destroy_all
 Event.destroy_all
 User.destroy_all
 
@@ -24,11 +23,9 @@ user3 = User.create!(
   last_name: "Kadjar",
   password: "azerty"
 )
-
 puts 'Finished!'
 
 puts 'Creating Events...'
-
 event1 = Event.create!(
   name: "Foire au Dindon",
   description: "Venez découvrir nos beaux Dindons de la région. Bien nourris, bien dodus, ils gloussent du matin au soir. Mais ne vous méprenez pas, leur timbre est doux et délicat! Ils régaleront aussi bien vos yeux que vos papilles !",
@@ -36,6 +33,7 @@ event1 = Event.create!(
   price: "10",
   start_at: Date.today,
   end_at: Date.tomorrow,
+  phone_number: "01 48 65 85 96",
   user: user1
 )
 
@@ -46,6 +44,7 @@ event2 = Event.create!(
   price: "5",
   start_at: Date.yesterday,
   end_at: Date.today,
+  phone_number: "01 75 49 63 52",
   user: user2
 )
 
@@ -56,11 +55,33 @@ event3 = Event.create!(
   price: "20",
   start_at: Date.today,
   end_at: Date.tomorrow,
+  phone_number: "06 99 58 42 56",
   user: user3
 )
-
 puts 'Finished!'
 
+puts 'Creating bookings'
+Booking.create!(
+  user_id: user1.id,
+  event_id: event2.id,
+  rating: "4",
+  status: "accepting",
+  reviews: "45"
+)
 
+Booking.create!(
+  user_id: user2.id,
+  event_id: event3.id,
+  rating: "5",
+  status: "pending",
+  reviews: "16"
+)
 
-
+Booking.create!(
+  user_id: user3.id,
+  event_id: event1.id,
+  rating: "3",
+  status: "dinied",
+  reviews: "24"
+)
+puts 'Finished!'
