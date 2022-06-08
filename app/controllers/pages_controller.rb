40ccce776a4a9e116de7218fa205a9e340250746
Alice Palussiere <journalice@gmail.com>
2022-06-08 10:09:26 +0200

    search fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,15 @@ class PagesController < ApplicationController
   def dashboard
     @created_events = current_user.events
     @participating_events = current_user.participating_events
+    @markers = @participating_events.geocoded.map do |event|
+      {
+        lat: event.latitude,
+        lng: event.longitude,
+        color: $lavande,
+        draggable: true,
+        info_window: render_to_string(partial: "events/info_window", locals: {event: event}),
+        image_url: helpers.asset_url("marqueur.png")
+      }
+    end
   end
 end
